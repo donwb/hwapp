54175bf0e33bc2dcfda86e7891ce53d054c25a5a
don bronwing <don.browning@gmail.com>
2016-11-21 00:40:07 -0500

    EF starting to make more sense now

--- Program.cs
+++ Program.cs
@@ -13,12 +13,23 @@ namespace hwapp
 
             using (var db = new mapContext()){
 
+                // Collection of UserActions
                 var users = db.Useractions.Include(mu => mu.User)
                 .Include(act => act.Actions)
                 .ToList();
                 
                 foreach (var u in users) {
                     Console.WriteLine(u.User.Email + " ----- " + u.Actions.Action);
+                }
+
+                // Go  through the MapUsers to then get the Actions
+                var fromUsers = db.Mapuser.Include(ua => ua.Useractions)
+                .ToList();
+                foreach (var u in fromUsers) {
+                    Console.WriteLine(u.Email);
+                    foreach(var a in u.Useractions){
+                        Console.WriteLine("\t" + a.Actions.Action);
+                    }
                 }
             }
             
