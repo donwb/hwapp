0a51f5e4d4b7b1014e0c7de261122a9b5121148f
don bronwing <don.browning@gmail.com>
2016-11-21 01:05:54 -0500

    org'd code for sanity

--- Program.cs
+++ Program.cs
@@ -13,42 +13,55 @@ namespace hwapp
 
             using (var db = new mapContext()){
 
-                // Collection of UserActions
-                var users = db.Useractions
-                .Include(mu => mu.User)
-                .Include(act => act.Actions)
-                .ToList();
-                
-                foreach (var u in users) {
-                    Console.WriteLine(u.User.Email + " ----- " + u.Actions.Action);
-                }
+                AllUserActions(db);
 
-                // Go  through the MapUsers to then get the Actions
-                var fromUsers = db.Mapuser
-                .Include(ua => ua.Useractions)
-                .ToList();
-                
-                foreach (var u in fromUsers) {
-                    Console.WriteLine(u.Email);
-                    foreach(var a in u.Useractions){
-                        Console.WriteLine("\t" + a.Actions.Action);
-                    }
-                }
+                ViaMapUsers(db);
 
-                // Just me
-                var me = db.Mapuser
-                .Include(ua => ua.Useractions)
-                .Single(u => u.Email.ToString() == "don.browning@turner.com");
-                
-                Console.WriteLine(me.Email);
-                foreach (var a in me.Useractions) {
-                    Console.WriteLine("\t" + a.Actions.Action);
-                }
+                JustMe(db);
+
 
             }
             
             Console.WriteLine("Done...");
             Console.ReadLine();
+        }
+
+        private static void AllUserActions(mapContext db) {
+            // Collection of UserActions
+            var users = db.Useractions
+            .Include(mu => mu.User)
+            .Include(act => act.Actions)
+            .ToList();
+            
+            foreach (var u in users) {
+                Console.WriteLine(u.User.Email + " ----- " + u.Actions.Action);
+            }
+        }
+
+        private static void ViaMapUsers(mapContext db) {
+            // Go  through the MapUsers to then get the Actions
+            var fromUsers = db.Mapuser
+            .Include(ua => ua.Useractions)
+            .ToList();
+            
+            foreach (var u in fromUsers) {
+                Console.WriteLine(u.Email);
+                foreach(var a in u.Useractions){
+                    Console.WriteLine("\t" + a.Actions.Action);
+                }
+            }
+        }
+
+        private static void JustMe(mapContext db) {
+            // Just me
+            var me = db.Mapuser
+            .Include(ua => ua.Useractions)
+            .Single(u => u.Email.ToString() == "don.browning@turner.com");
+            
+            Console.WriteLine(me.Email);
+            foreach (var a in me.Useractions) {
+                Console.WriteLine("\t" + a.Actions.Action);
+            }
         }
     }
 }
